dae01d6c3537576da451cb07433c359ee87b587e
lokigem <holmes0306@naver.com>
2024-09-04 20:35:30 +0900

    create project

---
+++ src/main/java/org/rookedsysc/coutableuuid/CoutableUuidApplication.java
@@ -0,0 +1,13 @@
+package org.rookedsysc.coutableuuid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoutableUuidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoutableUuidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rookedsysc/coutableuuid/CoutableUuidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rookedsysc.coutableuuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoutableUuidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
